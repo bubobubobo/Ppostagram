224dcddf5f632e9d88d2d7a8cc26b1d7f4d27a04
Chae SJ <phyzee@naver.com>
2021-06-29 02:32:53 +0900

    have some issues on post position

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import './App.css'
 import Post from './Post'
-import { db } from './firebase'
+import { db, auth } from './firebase'
 import { makeStyles } from '@material-ui/core/styles'
 import Modal from '@material-ui/core/Modal'
 import { Button, Input } from '@material-ui/core'
+import ImageUpload from './ImageUpload'
+import InstagramEmbed from 'react-instagram-embed'
 
 function getModalStyle() {
   const top = 50;
@@ -35,16 +37,46 @@ function App() {
 
   const [posts, setPosts] = useState([]);
   const [open, setOpen] = useState(false);
+  const [openSignIn, setOpenSignIn] = useState(false);
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
-  
+  const [user, setUser] = useState(null);
+
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => { // 이건 state가 refresh되어도 정보 유지
+      if (authUser) {
+        // user has logged in...
+        console.log(authUser);
+        setUser(authUser);
+
+        if (authUser.displayName) {
+          // dont update username
+        } else {
+          // uf we just created someone
+          return authUser.updateProfile({
+            displayName: username
+          });
+        }
+      } else {
+        // user has logged out...
+        setUser(null);
+      }
+    })
+
+    return () => {
+      // perform some cleanup actions before useeffect
+      unsubscribe();
+    }
+  }, [user, username]); // listening change of user & username
+
 
   // useEffect : Runs a piece of code based on a specific condition
 
   useEffect(() => {
     // This is where code runs
-    db.collection('posts').onSnapshot(snapshot => { // very powerfull listener
+    db.collection('posts').orderBy('timestamp', 'desc').onSnapshot(snapshot => { // very powerfull listener
       // every time a new post is added, this code fires.
       setPosts(snapshot.docs.map(doc => ({
         id: doc.id,
@@ -55,7 +87,27 @@ function App() {
 
 
   const signUp = (event) => {
+    event.preventDefault();
 
+    auth.createUserWithEmailAndPassword(email, password)
+    .then((authUser) => {
+      return authUser.user.updateProfile({
+        displayName: username
+      })
+    })
+    .catch((error) => alert(error.message));
+
+    setOpen(false);
+  }
+
+  const signIn = (event) => {
+    event.preventDefault();
+
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .catch((error) => alert(error.message))
+
+      setOpenSignIn(false);
   }
 
   return (
@@ -65,45 +117,105 @@ function App() {
         onClose={() => setOpen(false)}
       >
         <div style={modalStyle} className={classes.paper}>
+        <form className="app__signup">
           <center>
             <img className="app__headerImage" src="/image/logo.png" alt=""/>
-
-            <Input
-              placeholder="username"
-              type="text"
-              value={email}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <Input
-              placeholder="email"
-              type="text"
-              value={email}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <Input
-              placeholder="password"
-              type="password"
-              value={email}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <Button onClick={signUp}>Sign Up</Button>
-
-
           </center>
+          <Input
+            placeholder="username"
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <Input
+            placeholder="email"
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            placeholder="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button type="submit" onClick={signUp}>Sign Up</Button>
+        </form>
+        </div>
+      </Modal>
+
+      <Modal
+        open={openSignIn}
+        onClose={() => setOpenSignIn(false)}
+      >
+        <div style={modalStyle} className={classes.paper}>
+        <form className="app__signup">
+          <center>
+            <img className="app__headerImage" src="/image/logo.png" alt=""/>
+          </center>
+          <Input
+            placeholder="email"
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            placeholder="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button type="submit" onClick={signIn}>Sign In</Button>
+        </form>
         </div>
       </Modal>
       
       <div className="app__header">
         <img className="app__headerImage" src="/image/logo.png" alt=""/>
+        {user ? (
+          <Button onClick={() => auth.signOut()}>Logout</Button>
+        ): (
+          <div className="app__loginContainer">
+            <Button onClick={() => setOpenSignIn(true)}>Sign In</Button>
+            <Button onClick={() => setOpen(true)}>Sign Up</Button>
+          </div>
+        )}
       </div>
 
-      <Button onClick={() => setOpen(true)}>Sign Up</Button>
+      <div className="app__posts">
+        {
+          posts.map(({id, post}) => ( // everytime we get a post
+            <Post key={id} username={post.username} caption={post.caption} imageUrl={post.imageUrl}/>
+          ))
+        }
+      </div>
 
-      {
-        posts.map(({id, post}) => ( // everytime we get a post
-          <Post key={id} username={post.username} caption={post.caption} imageUrl={post.imageUrl}/>
-        ))
-      }
+      <InstagramEmbed
+      clientAccessToken='<appId>|<clientToken>'
+      url='https://instagr.am/p/Zw9o4/'
+      maxWidth={375}
+      hideCaption={false}
+      containerTagName='div'
+      injectScript
+      protocol=''
+      onLoading={() => {}}
+      onSuccess={() => {}}
+      onAfterRender={() => {}}
+      onFailure={() => {}}
+      />
+
+
+
+      {user?.displayName ? (
+        <ImageUpload username={user.displayName}/>
+      ): (
+        <h3>Sorry you need to login to upload</h3>
+      )}
+
+
+
+      
+
     </div>
   );
 }
